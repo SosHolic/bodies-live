9b70bfad1ed33fcc736ec0983299031e7348ad1b
Samuel Dobsa <dobsasamuel5@gmail.com>
2018-04-20 12:12:56 +0200

    src, main

---
+++ src/main/java/live/bodies/Activity.java
@@ -0,0 +1,14 @@
+public class Activitay {
+	
+	private long id;
+	private String firstName; 
+	private String lastName;
+
+	 new Activity ()
+		.set id(1)
+		.set firstName("JEHO")
+		.set lastName("MENO")
+	
+
+
+}
